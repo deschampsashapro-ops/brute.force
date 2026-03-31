137ba4c22c3e2ac29ade6e94e144a5ba55d9a8d0
deschampsashapro-ops <deschampsashapro@gmail.com>
2026-03-31 21:28:08 +0200

    SCORPION.py

--- SCORPION.py
+++ SCORPION.py
@@ -1,34 +1,38 @@
-import pygame
+import pygame  # On importe pygame pour faire un jeu / interface graphique
 
-pygame.init()
+pygame.init()  # On démarre pygame 
 
+# On charge l'image 
 tete_mort = pygame.image.load("C:\Users\sasha\SCORPION\tetedemort.png").convert_alpha()
 
-nouvelletaille = (100, 100)
+nouvelletaille = (100, 100)  # Nouvelle taille de l'image 
 
+# On redimensionne l'image
 tete_mort = pygame.transform.scale(tete_mort, nouvelletaille)
 
-size = (1000, 1000)
-screen = pygame.display.set_mode(size)
+size = (1000, 1000)  # Taille de la fenêtre 
+screen = pygame.display.set_mode(size)  # On crée la fenêtre
 
-pygame.display.set_caption("SCORPION")
+pygame.display.set_caption("SCORPION")  # Nom de la fenêtre 
 
+# Rectangle qui sert de zone de texte en bas
 textenter = pygame.Rect(350, 950, 300, 50)
 
-running = True
+running = True  # Variable pour faire tourner le jeu en boucle
+
+# Boucle principale
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    for event in pygame.event.get():  # On check tous les événements (clic, fermeture, etc.)
+        if event.type == pygame.QUIT:  # Si on clique sur la croix
+            running = False  # On arrête le jeu
 
-    screen.fill((139, 0, 0))
+    screen.fill((139, 0, 0))  #fond rouge sombre
 
-    screen.blit(tete_mort, (100, 100))
+    screen.blit(tete_mort, (100, 100))  # On affiche la tête de mort 
 
-    pygame.draw.rect(screen, (255, 255, 255), textenter)
+    pygame.draw.rect(screen, (255, 255, 255), textenter)  # On dessine un rectangle blanc
 
-    pygame.display.flip()
+    pygame.display.flip()  # On met à jour l'écran
 
-pygame.quit()
-
+pygame.quit()  # On ferme pygame
 
